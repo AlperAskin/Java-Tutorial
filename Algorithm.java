6074a1e506cd1166b75feb8d5ead393c6f1c1777
Salih Alper AŞKIN <39887084+AlperAskin@users.noreply.github.com>
2021-12-29 18:07:14 +0300

    Update Algorithm.java

--- Algorithm.java
+++ Algorithm.java
@@ -3,32 +3,24 @@ package algorithm;
 import java.util.Scanner;
 import static jdk.nashorn.internal.objects.NativeRegExp.test;
 
-public class Algorithm {
-    
+public class Algorithm {    
     public static String compress(String text,int n){
         char string[] = text.toCharArray();
-
         for(int i=0;i<string.length;i++) {
             int count=1;
             for(int j=i+1;j<string.length;j++){
-
                 if(string[i] == string[j] && string[i] != ' ')
                     count++;
             }
-
             if(count>=n)
                 text=text.replace(String.valueOf(string[i]),"");
         }
         return text;
-
     }
-
 public static void main(String[] args) {
-
        /* String test="qwertyyaacvxczasdfh";
         test=compress(test,2);
-        System.out.println(test);*/
-       
+        System.out.println(test);*/      
        String test= "assdddffffgggghhhhh";
        System.out.println("Ornek string: " +test);
        Scanner in = new Scanner(System.in);
@@ -36,19 +28,5 @@ public static void main(String[] args) {
        int n  = in.nextInt();
        test=compress(test, n);
        System.out.print("Son Durum" +test);
-       
-
-
-        
-
-     
-
-
-    
-       
-       
-       
-        
-
-    }
+       }
 }
